91e95b7836d37d7d6756e397ddec2e2535caeace
RamboboZ88 <82100284+RamboboZ88@users.noreply.github.com>
2022-03-09 17:03:41 +0100

    Corrections

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-//require_once __DIR__ .'/vendor/autoload.php';
+require_once __DIR__ .'/vendor/autoload.php';
 $config = require_once __DIR__ . "/src/conf/settings.php";
 
 use charlyday\controleurs\ControleurAccueil;
