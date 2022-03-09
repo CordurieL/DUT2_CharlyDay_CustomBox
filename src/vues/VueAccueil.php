e7060c3291d960ff642cfcf73ab2a6b78d6e77e2
Michel Grolet <michelgrolet@gmail.com>
2022-03-09 22:17:41 +0100

    Update VueAccueil.php

--- src/vues/VueAccueil.php
+++ src/vues/VueAccueil.php
@@ -1,55 +1,57 @@
-<?php declare(strict_types = 1);
+<?php declare(strict_types=1);
 
 namespace charlyday\vues;
 
 use charlyday\vues\Vue;
 use Slim\Container;
 
-class VueAccueil extends Vue
-{	
+class VueAccueil extends Vue {
 	/**
-	* Accueil
-	*/
-	private function render_displayAccueil() :String{
+	 * Accueil
+	 */
+	private function render_displayAccueil(): string {
 		$html = "
 			<h1>Accueil</h1>
-			<form action='".$this->container->router->pathFor('test')."' method='GET'>
+			<form action='" . $this->container->router->pathFor('test') . "' method='GET'>
 				<input type='submit' value='Test'>
 			</form>
 		";
-        return $html;
+		return $html;
 	}
 
-	private function render_displayTest() :String{
+	private function render_displayTest(): string {
 		$html = "
 			<h1>Test</h1>
 		";
 		return $html;
 	}
-	
-	public function render($selecteur) :String{
+
+	public function render($selecteur): string {
 		switch ($selecteur) {
-			case 1 : {
+			case 1 :
+			{
 				$content = $this->render_displayAccueil();
 				break;
 			}
-			case 2 : {
+			case 2 :
+			{
 				$content = $this->render_displayTest();
 				break;
 			}
-			default : {
+			default :
+			{
 				$content = "Pas de contenu<br>";
 				break;
 			}
 		}
 
-		return 
-		"<!DOCTYPE html>
+		return <<<END
+		<!DOCTYPE html>
 
 		<html lang='fr'>
 			<head>
-				<meta charset=\"utf-8\"/>
-				<link rel=\"stylesheet\" media=\"screen\" type=\"text/css\" href=\"web/css/style.css\"/>
+				<meta charset='utf-8'>
+				<link rel='stylesheet' media='screen' type='text/css' href='web/css/style.css'/>
 				<title>sometext</title>
 			</head>
 			<body>
@@ -59,13 +61,14 @@ class VueAccueil extends Vue
 					</nav>
 				</header>
 				
-                <div class=\"content\">
+				<div class='content'>
 					$content
 				</div>
 				<footer>
 
 				</footer>
 			</body>
-		<html>";
+		<html lang='fr'>
+		END;
 	}
 }
