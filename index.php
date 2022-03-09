4555d8e553bb2526c0e23d9288647f2ff7dc0445
Michel Grolet <michelgrolet@gmail.com>
2022-03-09 16:09:36 +0100

    jytryj

--- index.php
+++ index.php
@@ -1,1 +1,31 @@
 <?php
+
+require_once 'vendor/autoload.php';
+
+session_start();
+
+$configuration = [
+	'settings' => [
+		'displayErrorDetails' => true,
+		'dbconf' => './src/conf.ini']];
+
+$c = new Container($configuration);
+$app = new App($c);
+$db = new DB();
+$db->addConnection(parse_ini_file($configuration['settings']['dbconf']));
+$db->setAsGlobal();
+$db->bootEloquent();
+
+
+
+// Routes ////////////////////////////
+$app->get('/', MainController::class . ':afficherAccueil')->setName('Accueil');
+
+
+
+/////////////////////////////////////
+
+
+// Lancement de l'application
+try	$app->run();
+catch (Throwable $e) echo $e->getMessage();
