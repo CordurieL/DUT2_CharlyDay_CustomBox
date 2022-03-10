4a5c234e3ebe3a411466c100d382311636a8587a
CordurieL <82098372+CordurieL@users.noreply.github.com>
2022-03-10 11:30:09 +0100

    vue produits + petit bout css

--- src/vue/VueProduit.php
+++ src/vue/VueProduit.php
@@ -4,31 +4,59 @@ namespace custombox\vue;
 
 use Slim\Container;
 
-class VueProduit {
-	protected $objet;
-	protected $container;
+class VueProduit
+{
+    protected $objet;
+    protected $container;
 
-	public function __construct(Container $c, $ob = null) {
-		$this->container = $c;
-		$this->objet = $ob;
-	}
+    public function __construct(Container $c, $ob = null)
+    {
+        $this->container = $c;
+        $this->objet = $ob;
+    }
 
-	public function render($selecteur): string {
-		switch ($selecteur) {
-			default :
-			{
-				$content = "Pas de contenu<br>";
-				break;
-			}
-		}
+    public function displayProducts() : string
+    {
+        $products = $this->objet;
+        $content = "
+		<input type=\"text\" id=\"rechercheProduit\" placeholder=\"Rechercher un produit\"/>
+		<div id=\"products-list\">
+		<div class='productLine' id='productLineRef'>
+			<span class='productLineTitle'>Nom de produit</span>
+			<span = class='productLineDescr'>Descriptif</span>
+			<span class ='productLineCateg'>Catégorie</span>
+			<span class='productLineWeight'>Poids</span>
+		</div><br><hr><br>";
+        foreach ($products as $p) {
+            $content .= "<div class='productLine'><span class='productLineTitle'>$p[titre]</span><span = class='productLineDescr'>$p[description]</span><span class ='productLineCateg'>$p[categorie]</span><span class='productLineWeight'>$p[poids]</span></div><br>";
+        }
+        $content .= "</div>";
+        return $content;
+    }
 
-		return
-			"<!DOCTYPE html>
+    public function render($selecteur): string
+    {
+        $content = "";
+        switch ($selecteur) {
+            case 1:
+            {
+                $content = $this->displayProducts();
+                break;
+            }
+            default:
+            {
+                $content = "Pas de contenu<br>";
+                break;
+            }
+        }
+
+        return
+            "<!DOCTYPE html>
 
 		<html lang='fr'>
 			<head>
 				<meta charset=\"utf-8\"/>
-				<link rel=\"stylesheet\" media=\"screen\" type=\"text/css\" href=\"web/css/style.css\"/>
+				<link rel=\"stylesheet\" media=\"screen\" type=\"text/css\" href=\"../assets/css/style.css\"/>
 				<title>sometext</title>
 			</head>
 			<body>
@@ -46,5 +74,5 @@ class VueProduit {
 				</footer>
 			</body>
 		<html>";
-	}
+    }
 }
