9306592fa6df66d08a520a9cb48edc99791d16c2
CordurieL <82098372+CordurieL@users.noreply.github.com>
2022-03-10 13:18:04 +0100

    catalogue maj catégories

--- src/controleurs/ControleurProduit.php
+++ src/controleurs/ControleurProduit.php
@@ -1,9 +1,11 @@
-<?php declare(strict_types=1);
+<?php
+declare(strict_types=1);
 
 namespace custombox\controleurs;
 
 use custombox\exceptions\AuthException;
 use custombox\models\Produit;
+use custombox\models\Categorie;
 use custombox\vue\VueAccount;
 use custombox\vue\VueProduit;
 use custombox\controleurs\Controleur;
@@ -12,24 +14,25 @@ use Slim\Container;
 use Slim\Http\Request;
 use Slim\Http\Response;
 
-class ControleurProduit extends Controleur {
-	public function __construct(Container $c) {
-		parent::__construct($c);
-	}
+class ControleurProduit extends Controleur
+{
+    public function __construct(Container $c)
+    {
+        parent::__construct($c);
+    }
 
-	/**
-	 * Affichage de l ensemble des produits
-	 */
-	function displayProducts(Request $rq, Response $rs, array $args): Response {
-		$products = Produit::all();
-		$v = new VueProduit($this->container, $products);
-		$rs->getBody()->write($v->render(1));
-		return $rs;
-	}
+    /**
+     * Affichage de l ensemble des produits
+     */
+    public function displayProducts(Request $rq, Response $rs, array $args): Response
+    {
+        $products = Produit::all();
+        foreach ($products as $p) {
+            $categString = Categorie::where("id", "=", $p->categorie)->get("nom")->toArray()[0]["nom"];
+            $p["categorie"] = $categString;
+        }
+        $v = new VueProduit($this->container, $products);
+        $rs->getBody()->write($v->render(1));
+        return $rs;
+    }
 }
-
-
-
-
-
-
