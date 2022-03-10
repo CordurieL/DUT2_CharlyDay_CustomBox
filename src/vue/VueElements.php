63d1f432d9d7d43f4cb40c40ad4af62fa1ca6e4c
unshade <72690981+unshade@users.noreply.github.com>
2022-03-10 15:42:55 +0100

    fix routes css

--- src/vue/VueElements.php
+++ src/vue/VueElements.php
@@ -10,7 +10,7 @@ class VueElements extends Vue {
 		<html lang='fr'>
 			<head>
 				<meta charset="UTF-8"/>
-				<link rel="stylesheet" href="assets/styles/css/main.min.css"/>
+				<link rel="stylesheet" href="/assets/styles/css/main.min.css"/>
 				  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 				<title>$title</title>
 			</head>
