63df5185f57addbc6fb2cc132242082abcd22229
RamboboZ88 <82100284+RamboboZ88@users.noreply.github.com>
2022-03-10 17:43:31 +0100

    debug

--- src/controleurs/ControleurBoite.php
+++ src/controleurs/ControleurBoite.php
@@ -26,18 +26,17 @@ class ControleurBoite extends Controleur {
 	 */
 	public function createBox(Request $rq, Response $rs, array $args): Response {
 		$container = $this->container;
+		$param=$rq->getParsedBody();
+		$box=new Boite();
 
 		$userid = $_SESSION['id_user'];
 		if(isset($param['id_user'])){
-            $publique = true;
+            $box->createBox($param['taille'],$param['couleur'],$param['message'],$param['id_user']);
         }
         else{
-            $publique = false;
+            $box->createBox($param['taille'],$param['couleur'],$param['message']);
         }
 		
-		$param=$rq->getParsedBody();
-		$box=new Boite();
-		$box->createBox($param['taille'],$param['couleur'],$param['message']);
 		$v = new VueBoite($this->container,$box);
 		$rs->getBody()->write($v->render(2)) ;
 		
@@ -69,6 +68,24 @@ class ControleurBoite extends Controleur {
 		
 		return $rs ;
 	}
+	
+	/**
+	* Permet d'afficher toutes les boites d'un compte
+	*/
+	public function listesBoites(Request $rq, Response $rs, array $args) :Response{
+		$container = $this->container ;
+		
+		$userid = $_SESSION['id_user'];
+		if(isset($param['id_user'])){
+            $items=Boite->where('id_user','=',$param['id_user'])->get();
+			$v = new VueBoite($this->container,$items);
+			$rs->getBody()->write($v->render(4)) ;
+        }
+		
+		return $rs ;
+	}
+	
+	
 }
 
 
--- src/vue/VueBoite.php
+++ src/vue/VueBoite.php
@@ -45,6 +45,27 @@ class VueBoite extends Vue {
 			</form></div>";
 		return $res;
 	}
+	
+	/**
+	* Listes des boites d'un compte
+	*/
+	private function render_listesBoites():String{
+		if($this->objet!==null){
+			$res="<section><ol>Toutes vos boites :";
+			foreach($this->objet as $l){
+				$res=$res."<li><a href=\"".
+				$this->container->router->pathFor('listesPersos')."\">
+				<p>Numéro : $l->id_boite Etat : $l->etat
+				Message : $l->message Poids max : $l->poidsmax Taille : $l->taille</p></li></a>";
+			}
+			$res=$res."</ol></section>";
+		}
+		else{
+			$res="<section><p>Il n'y a actuellement aucune boite.</p></section>";
+		}
+
+		return $res;
+	}
 
 	public function render($selecteur): string {
 		switch ($selecteur) {
@@ -58,6 +79,10 @@ class VueBoite extends Vue {
 			}
 			case 3 : {
 				$content = $this->render_formBox();
+				break;
+			}
+			case 4 : {
+				$content = $this->render_listesBoites();
 				break;
 			}
 			default :
